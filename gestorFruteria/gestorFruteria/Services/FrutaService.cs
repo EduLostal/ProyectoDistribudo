2711c066f56cddd6cf0aa75bacde454ce0fad1cc
MOROMEM <sergio.moro@alumnos.uneatlantico.es>
2023-12-17 20:31:15 +0100

    Actualizacion put

--- gestorFruteria/gestorFruteria/Services/FrutaService.cs
+++ gestorFruteria/gestorFruteria/Services/FrutaService.cs
@@ -54,12 +54,43 @@ namespace TuAplicacion.Services
             await _dbContext.Frutas.InsertOneAsync(nuevaFruta);
         }
         //Modificacion fruta
-        public async Task ModificacionAsync(ObjectId id,Fruta actualizaFruta)
-        {                       
-            actualizaFruta.Id = id;
+        public async Task ModificacionAsync(ObjectId id,Fruta cambiosFruta)
+        {
+            var update = Builders<Fruta>.Update;
+            var updateDef = new List<UpdateDefinition<Fruta>>();
 
-           
-            await _dbContext.Frutas.ReplaceOneAsync(f => f.Id == id, actualizaFruta);
+            if (!string.IsNullOrEmpty(cambiosFruta.NombreFruta))
+                updateDef.Add(update.Set(f => f.NombreFruta, cambiosFruta.NombreFruta));
+
+            if (cambiosFruta.Cantidad > 0)
+                updateDef.Add(update.Set(f => f.Cantidad, cambiosFruta.Cantidad));
+
+            if (cambiosFruta.PrecioPorUnidad > 0)
+                updateDef.Add(update.Set(f => f.PrecioPorUnidad, cambiosFruta.PrecioPorUnidad));
+            if(!string.IsNullOrEmpty(cambiosFruta.Proveedor.Nombre))
+                updateDef.Add(update.Set(f => f.Proveedor.Nombre, cambiosFruta.Proveedor.Nombre));
+            if (!string.IsNullOrEmpty(cambiosFruta.Proveedor.Contacto))
+                updateDef.Add(update.Set(f => f.Proveedor.Contacto, cambiosFruta.Proveedor.Contacto));
+            if (!string.IsNullOrEmpty(cambiosFruta.Proveedor.Telefono))
+                updateDef.Add(update.Set(f => f.Proveedor.Telefono, cambiosFruta.Proveedor.Telefono));
+            if (!string.IsNullOrEmpty(cambiosFruta.Venta.Cliente))
+                updateDef.Add(update.Set(f => f.Venta.Cliente, cambiosFruta.Venta.Cliente));
+            if (cambiosFruta.Venta.CantidadVendida >0)
+                updateDef.Add(update.Set(f => f.Venta.CantidadVendida, cambiosFruta.Venta.CantidadVendida));
+            if (cambiosFruta.Venta.PrecioVenta > 0)
+                updateDef.Add(update.Set(f => f.Venta.PrecioVenta, cambiosFruta.Venta.PrecioVenta));
+            if (cambiosFruta.Venta.FechaVenta > DateTime.MinValue)
+                updateDef.Add(update.Set(f => f.Venta.FechaVenta, cambiosFruta.Venta.FechaVenta));
+            
+
+            if (updateDef.Count > 0)
+            {
+                var combinedUpdate = update.Combine(updateDef);
+                await _dbContext.Frutas.UpdateOneAsync(f => f.Id == id, combinedUpdate);
+            }
+
+
+        
 
         }
 
